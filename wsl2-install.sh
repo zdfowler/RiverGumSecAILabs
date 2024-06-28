75a30e01ab491d0160f63872be136ed9225b5462
Joff Thyer <joff@blackhillsinfosec.com>
2024-06-28 14:41:19 -0400

    minor fixes

--- wsl2-install.sh
+++ wsl2-install.sh
@@ -2,5 +2,5 @@
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 /bin/bash Miniconda3-latest-Linux-x86_64.sh -b
-./miniconda3/bin/conda init
+$HOME/miniconda3/bin/conda init
 
