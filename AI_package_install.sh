9909cf71161bdbc29fc6a0d95a3df4bd1cbf0c7a
Joff Thyer <joff@blackhillsinfosec.com>
2024-06-28 15:43:54 -0400

    script rename

--- AI_package_install.sh
+++ AI_package_install.sh
@@ -39,6 +39,7 @@ if [[ `uname -r` == *"WSL2"* ]]; then
 	conda install -yq cudatoolkit >/dev/null 2>&1
 	echo "[+] Installing: tensorflow[and-cuda] for WSL2"
 	pip install -q tensorflow[and-cuda] >/dev/null 2>&1
+	export LD_LIBRARY_PATH=`echo $(find $HOME/miniconda3/envs/$CONDA_DEFAULT_ENV/lib/python3.10/site-packages/nvidia -type d -name lib) | sed 's/ /:/g'`
 else
 	echo "[*] #######################################################"
 	echo "[*] ## Target System UNIX/MacOS/BSD: using conda method. ##"
