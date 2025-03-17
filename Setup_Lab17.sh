b5a4f13d01eded74f8ba24f457ae0670a13cdafa
Joff Thyer <joff@blackhillsinfosec.com>
2025-03-17 16:08:57 -0400

    added lab setup scripts

--- Setup_Lab17.sh
+++ Setup_Lab17.sh
@@ -2,19 +2,15 @@ if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
     echo "You must run this script using 'source ${0}'"
     exit 1
 fi
-LAB="lab22"
-LDIR="./022_LargeLanguageModels"
+LAB="lab17"
+LDIR="./017_SupervisedLearning"
 conda deactivate
 `conda env list | grep -q $LAB` && (echo "Removing Existing $LAB16 conda env" && conda env remove -q --yes -n $LAB)
-conda create -n lab22 \
-    python=3.11 \
-    pytorch-cuda=12.1 \
-    pytorch cudatoolkit xformers -c pytorch -c nvidia -c xformers \
-    -y
-conda activate lab22
-pip install unsloth
-pip install jupyter
+conda create --yes -n $LAB python=3.12
+conda activate $LAB
+pip install --upgrade poetry
 cd $LDIR
+poetry install --no-root
 echo "########################################################"
 echo " $LAB Setup is complete."
 echo " Start jupyter-notebook to proceed"
